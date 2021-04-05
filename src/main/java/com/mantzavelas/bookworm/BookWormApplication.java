254ab921d37cd82ff2133140b5e1cb9f7f44da3d
Kostas Mantzavelas <kostiksm@gmail.com>
2021-04-05 20:55:20 +0300

    Initial Commit

---
+++ src/main/java/com/mantzavelas/bookworm/BookWormApplication.java
@@ -0,0 +1,13 @@
+package com.mantzavelas.bookworm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mantzavelas/bookworm/BookWormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantzavelas.bookworm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
