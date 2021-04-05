1ebc61bbaf7cb8ea8a7da02e01dfb5f989f732a3
Kostas Mantzavelas <kostiksm@gmail.com>
2021-04-05 23:27:22 +0300

    - Introduce Author, Book, Publisher JPA entities - add lombok
    Issue #1

---
+++ src/main/java/com/mantzavelas/bookworm/models/Author.java
@@ -0,0 +1,31 @@
+package com.mantzavelas.bookworm.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private LocalDate dateOfBirth;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "author")
+    private List<Book> books;
+}
---
+++ src/main/java/com/mantzavelas/bookworm/models/Publisher.java
@@ -0,0 +1,28 @@
+package com.mantzavelas.bookworm.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String telephone;
+
+    private String address;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "publisher")
+    private List<Book> books;
+}
