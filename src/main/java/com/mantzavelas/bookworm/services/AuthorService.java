3744e16ed00d6df1e717c71601a3a751097f0531
Kostas Mantzavelas <kostiksm@gmail.com>
2021-04-07 15:40:21 +0300

    - Enhance AuthorController to expose endpoint to author's books
    Issue #20

--- src/main/java/com/mantzavelas/bookworm/services/AuthorService.java
+++ src/main/java/com/mantzavelas/bookworm/services/AuthorService.java
@@ -2,11 +2,17 @@ package com.mantzavelas.bookworm.services;
 
 import com.mantzavelas.bookworm.converters.AuthorResourceToAuthor;
 import com.mantzavelas.bookworm.converters.AuthorToAuthorResource;
+import com.mantzavelas.bookworm.converters.BookToBookResource;
 import com.mantzavelas.bookworm.exceptions.ResourceAlreadyExistsException;
+import com.mantzavelas.bookworm.exceptions.ResourceNotFoundException;
 import com.mantzavelas.bookworm.models.Author;
 import com.mantzavelas.bookworm.repositories.AuthorRepository;
 import com.mantzavelas.bookworm.resources.AuthorResource;
+import com.mantzavelas.bookworm.resources.BookResource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
@@ -14,11 +20,13 @@ public class AuthorService {
     private final AuthorRepository authorRepository;
     private final AuthorResourceToAuthor authorResourceConverter;
     private final AuthorToAuthorResource authorConverter;
+    private final BookToBookResource bookConverter;
 
-    public AuthorService(AuthorRepository authorRepository, AuthorResourceToAuthor authorResourceConverter, AuthorToAuthorResource authorConverter) {
+    public AuthorService(AuthorRepository authorRepository, AuthorResourceToAuthor authorResourceConverter, AuthorToAuthorResource authorConverter, BookToBookResource bookConverter) {
         this.authorRepository = authorRepository;
         this.authorResourceConverter = authorResourceConverter;
         this.authorConverter = authorConverter;
+        this.bookConverter = bookConverter;
     }
 
     public AuthorResource createNewAuthor(AuthorResource resource) {
@@ -35,5 +43,14 @@ public class AuthorService {
         if (authorRepository.existsByEmail(resource.getEmail())) {
             throw new ResourceAlreadyExistsException("Author already exists with email " + resource.getEmail());
         }
+    }
+
+    public List<BookResource> getBooks(Long authorId) {
+        return authorRepository.findById(authorId)
+            .orElseThrow(() -> new ResourceNotFoundException("Author not found with id " + authorId))
+            .getBooks()
+            .stream()
+            .map(bookConverter::convert)
+            .collect(Collectors.toList());
     }
 }
